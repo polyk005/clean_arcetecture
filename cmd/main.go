60fffe5344a4c52beae5781ca8f932deb88cd8a8
Midiy <polyk6859@gmail.com>
2025-05-10 16:07:11 +0300

    Apdate

---
+++ cmd/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"clean_architecture/cmd/bootstrap"
+	"time"
+
+	"github.com/amitshekhariitbhu/go-backend-clean-architecture/api/route"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	app := bootstrap.App()
+
+	env := app.Env
+
+	db := app.Mongo.Database(env.DBName)
+	defer app.CloseDBConnection()
+
+	timeout := time.Duration(env.ContextTimeout) * time.Second
+
+	gin := gin.Default()
+
+	route.Setup(env, timeout, db, gin)
+
+	gin.Run(env.ServerAddress)
+}
